68e16874326203618c08f34c85bc1dc2eb7426d5
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-20 13:42:39 -0400

    added basic page routes.

---
+++ controllers/profile-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { User, Interest, Activity } = require('../models');
+
+router.get('/', async (req, res) =>{
+    try {
+        res.render('profile');
+    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      } 
+})
+
+module.exports = router;
