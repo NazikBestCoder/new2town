db5a00fa8e4cb1d1751ed6668d301f6b02a49776
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-21 14:40:46 -0400

    added card generation based on users return by get request.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,7 +34,44 @@ router.get('/search/:act', async (req, res) =>{
 
       console.log(userAll);
 
+    //   res.render('partials/user-cards', {
+    //     logged_in: req.session.logged_in,
+    //     user_id: req.session.user_id,
+    //     userAll,
+    // });
+        //  res.render('partials/user-cards', {
+        // logged_in: req.session.logged_in,
+        // user_id: req.session.user_id,
+        // userAll,
       res.status(200).json(userAll);
+
+  
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    } 
+})
+
+router.get('/gen/:act', async (req, res) =>{
+  try {
+
+      console.log(req.params.act);
+
+      // const currentUserData = await User.findByPk(req.session.user_id);
+
+      // const currentUser = currentUserData.get({ plain: true });
+
+      const userData = await User.findAll();
+      
+      const userAll = userData.map((user) => user.get({ plain: true }));
+
+      console.log("round 2!");
+
+      res.render('home', {
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id,
+      userAll,
+      })
   
   } catch (err) {
       console.log(err);
--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,9 @@
 const searchButtonEl = document.getElementById('search-btn');
 const activitySearchBarEl = document.getElementById('activity-search-bar');
-const test = document.getElementById('test');
+const resultAreaEl = document.getElementById('results-area');
 
 async function generateResultCards (){
+        let gottenUsers;
         const chosenAct = activitySearchBarEl.options[activitySearchBarEl.selectedIndex].value;
         const response = await fetch(`/search/${chosenAct}`, {
           method: 'GET',
@@ -13,10 +14,24 @@ async function generateResultCards (){
             const res = await response.json();
             console.log(response);
             console.log(res);
+            gottenUsers = res
         } else {
           alert('Failed to search.');
+        };
+
+        let cardsHtml = ``;
+
+        for (let i = 0; i < gottenUsers.length; i++){
+           cardsHtml += `</div>
+          <a href="/profile/${gottenUsers[i].id}"> <div class="card border-black border-2 rounded-lg inline-block w-52 h-64 mx-5">
+       <div class="container">
+           <h4><b>${gottenUsers[i].username}</b></h4>
+           <p>Interests: film, cooking, politics</p>
+       </div> </a>
+       </div>`
         }
-      
+
+        resultAreaEl.innerHTML = cardsHtml
 };
 
 searchButtonEl.addEventListener("click", (e) => {
