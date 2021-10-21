ec943fc078716301255a58614449488034332883
aorich <rich.aidan@gmail.com>
2021-10-21 17:37:45 -0400

    Friend's list working

--- controllers/friends-list-routes.js
+++ controllers/friends-list-routes.js
@@ -2,39 +2,40 @@ const router = require('express').Router();
 const { User, Interest, Activity } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/friendslist', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
 
     
-//     try {
+    try {
 
-//         const friendData = await User.findAll(
-//             // where: {
-//             //    friend_id: req.session.user_id,
-//             // }
-//         );
-//         console.log("hello")
-//         const friendAll = friendData.map((friend) => friend.get({ plain: true }));
+        const friendData = await User.findAll({
+            where: {
+               friend_id: req.session.user_id,
+            }
+        });
 
-//         res.render('friendslist', friendAll )
+        console.log(friendData)
+        const friendAll = friendData.map((friend) => friend.get({ plain: true }));
+
+        // res.render('friendslist', {friendAll} )
     
 
-//         // res.render('friendslist', {friendAll,
-//         //     logged_in: req.session.logged_in,
-//         // });
+        res.render('friendslist', {friendAll,
+            logged_in: req.session.logged_in,
+        });
     
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//       } 
-// });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      } 
+});
 
-// router.get('/login', (req, res) => {
-//     if (req.session.logged_in) {
-//       res.redirect('/');
-//       return;
-//     }
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
   
-//     res.render('login');
-//   });
+    res.render('login');
+  });
 
 module.exports = router;
