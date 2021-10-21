6fe082243b3fed31ef98cd6dc52e677966391981
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-21 11:10:56 -0400

    removed the first router.get from login and kept one route

--- controllers/login-routes.js
+++ controllers/login-routes.js
@@ -1,17 +1,7 @@
 const router = require('express').Router();
 const { User, Interest, Activity } = require('../models');
 
-router.get('/', async (req, res) =>{
-    try {
-        res.render('login');
-    
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-      } 
-})
-
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/');
       return;
