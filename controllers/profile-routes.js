1fdf999657c8868eecbdc957a74583f0c8182a8e
aorich <rich.aidan@gmail.com>
2021-10-22 09:28:13 -0400

    profile update location

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -22,6 +22,32 @@ router.get('/:user_id', withAuth, async (req, res) =>{
       }
 })
 
+router.put('/:user_id', withAuth, async, (req, res) => {
+
+  try {
+
+    const profileData = await User.update({
+      location: req.body.location,
+    },
+    {
+      where: {
+        id: req.session.user_id,
+      },
+    }
+    );
+
+    const profile = profileData.put({ plain: true });
+
+    res.render('myprofile', profile);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+});
+
+
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/');
