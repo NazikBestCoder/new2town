bbade2020b265a2066ae475d9634c51ee40a6a19
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-24 19:34:47 -0400

    newer styles.

--- public/js/home.js
+++ public/js/home.js
@@ -55,17 +55,21 @@ async function generateResultCards() {
     for (let i = 0; i < gottenUsers.length; i++) {
       let commonInterestsStr = '<ol>';
       gottenUsers[i].commonInterests.forEach((interest) => {
-        commonInterestsStr += `<li>${interest}</li>`;
+        commonInterestsStr += `<li class="card-interests text-lg">${interest}</li>`;
       });
       commonInterestsStr += `</ol>`;
       cardsHtml += `
          <div class="card top-0 border-black border-2 rounded-lg inline-block h-auto max-h-96 m-5">
-       <div class="container h-full relative">
-       <a href="/profile/other/${gottenUsers[i].id}"><h4><b>${gottenUsers[i].username}</b></h4></a>
-       <img style="width: 100px" src="${gottenUsers[i].photo_url}"
-           <p>Common Interests: ${commonInterestsStr}</p>
+       <div class="container h-full relative flex justify-center items-center flex-col">
+       <a href="/profile/other/${gottenUsers[i].id}"><h4><b class="text-3xl underline">${gottenUsers[i].username}</b></h4></a>
+       <br>
+       <img style="width: 100px" class="card-pic" src="${gottenUsers[i].photo_url}">
+       <br>
+           <p class="card-interests text-lg">Common Interests: ${commonInterestsStr}</p>
+           <br>
+           <br>
            <button id="friend-btn-${gottenUsers[i].id}" class="friend-btn hover:border-blue-200 hover:bg-gray-50 rounded w-40 h-10 border-black border-2 absolute bottom-0">Add Friend!</button>
-       </div> 
+           </div> 
        </div>`;
       addFriend(gottenUsers[i].id);
     }
