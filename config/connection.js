9fa67a7ff024714f67256298231e12ae6f6af05a
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-24 15:29:37 -0400

    reverted changes I made that did not work

--- config/connection.js
+++ config/connection.js
@@ -3,34 +3,15 @@ require('dotenv').config();
 // Passing parametres separately(mysql)
 const Sequelize = require('sequelize');
 
-let sequelize;
-
-if(process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
-
-
 
